cc43dc10adade006125eda91188c260e55163b99
Evgenii Rybalkin <eugeny.rybalkin@gmail.com>
2023-01-18 15:32:37 +0100

    add happy case test

--- src/test/java/cardDeliveryFormTest.java
+++ src/test/java/cardDeliveryFormTest.java
@@ -1,3 +1,31 @@
-public class cardDeliveryFormTest {
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Selenide.*;
+
+public class cardDeliveryFormTest {
+    @Test
+    public void happyPassCase() {
+        Configuration.holdBrowserOpen = true;
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        open("http://0.0.0.0:9999");
+        $x("//input[contains (@placeholder, 'Город' )]").setValue("Омск");
+        $x("//label[contains(@data-test-id, 'agreement')]").click();
+        $x("//* [contains(@placeholder , 'Дата встречи')]").click();
+        $x("//* [contains(@placeholder , 'Дата встречи')]").sendKeys(Keys.COMMAND+"A");
+        $x("//* [contains(@placeholder , 'Дата встречи')]").sendKeys(Keys.DELETE);
+        $x("//* [contains(@placeholder , 'Дата встречи')]").setValue(currentDate.plusDays(4).format(formatter));
+        $("[data-test-id='name'] input").setValue("Иванов Илья");
+        $("[data-test-id='phone'] input").setValue("+79436578935");
+        $x("//*[contains(text(), 'Забронировать')]").click();
+        $x("//*[contains(@class,'notification__content')]").should(appear, Duration.ofMillis(15000));
+    }
 }
