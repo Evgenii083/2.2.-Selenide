daeb3ca227d9171da876a54baf266af0defa5054
Evgenii Rybalkin <eugeny.rybalkin@gmail.com>
2023-01-19 23:57:23 +0100

    changed settings for CI, add test

---
+++ src/test/java/СardDeliveryFormTest.java
@@ -0,0 +1,51 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Selenide.*;
+
+public class СardDeliveryFormTest {
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    }
+
+    @Test
+    public void happyPassCase() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        $x("//input[contains (@placeholder, 'Город' )]").setValue("Омск");
+        $x("//label[contains(@data-test-id, 'agreement')]").click();
+        $x("//* [contains(@placeholder , 'Дата встречи')]").click();
+        $x("//* [contains(@placeholder , 'Дата встречи')]").sendKeys(Keys.COMMAND + "A");
+        $x("//* [contains(@placeholder , 'Дата встречи')]").sendKeys(Keys.DELETE);
+        $x("//* [contains(@placeholder , 'Дата встречи')]").setValue(generateDate(4, "dd-MM-yyyy"));
+        $("[data-test-id='name'] input").setValue("Иванов Илья");
+        $("[data-test-id='phone'] input").setValue("+79436578935");
+        $x("//*[contains(text(), 'Забронировать')]").click();
+        $x("//*[contains(@class,'notification__content')]").should(appear, Duration.ofMillis(15000));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + "23.01.2023"), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+    }
+
+    @Test
+    public void dropDownListCase() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        $x("//input[contains (@placeholder, 'Город' )]").setValue("сан");
+        $x("//* [contains(text() , 'Санкт-Петербург')]").click();
+        $x("//span[contains(@class , 'input__icon')]").click();
+        $x("//*[contains(@data-day, '1674687600000')]").click();
+        $("[data-test-id='name'] input").setValue("Иванов Илья");
+        $("[data-test-id='phone'] input").setValue("+79436578935");
+        $x("//label[contains(@data-test-id, 'agreement')]").click();
+        $x("//*[contains(text(), 'Забронировать')]").click();
+        $x("//*[contains(@class,'notification__content')]").should(appear, Duration.ofMillis(15000));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + "26.01.2023"), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+
+    }
+}
