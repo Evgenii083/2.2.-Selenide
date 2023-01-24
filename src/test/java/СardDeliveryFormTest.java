24713edb1b5c3b0697f88c15376926f85dab5a9d
Evgenii Rybalkin <eugeny.rybalkin@gmail.com>
2023-01-24 18:17:58 +0100

    changed test logic

--- src/test/java/СardDeliveryFormTest.java
+++ src/test/java/СardDeliveryFormTest.java
@@ -11,6 +11,7 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Selenide.*;
 
+
 public class СardDeliveryFormTest {
     public String generateDate(long addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
@@ -27,8 +28,7 @@ public class СardDeliveryFormTest {
         $x("//input[contains (@placeholder, 'Город' )]").setValue("Омск");
         $x("//label[contains(@data-test-id, 'agreement')]").click();
         $x("//* [contains(@placeholder , 'Дата встречи')]").click();
-        $x("//* [contains(@placeholder , 'Дата встречи')]").sendKeys(Keys.COMMAND + "A");
-        $x("//* [contains(@placeholder , 'Дата встречи')]").sendKeys(Keys.DELETE);
+        $x("//* [contains(@placeholder , 'Дата встречи')]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $x("//* [contains(@placeholder , 'Дата встречи')]").setValue(generateDate(4, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").setValue("Иванов Илья");
         $("[data-test-id='phone'] input").setValue("+79436578935");
@@ -39,17 +39,24 @@ public class СardDeliveryFormTest {
 
     @Test
     public void dropDownListCase() {
+        int currentDate = Integer.parseInt(generateDate(3, "MM"));
+        int planingDate = Integer.parseInt(generateDate(7, "MM"));
+        int planingDay = Integer.parseInt(generateDate(7, "dd"));
         $x("//input[contains (@placeholder, 'Город' )]").setValue("сан");
         $x("//* [contains(text() , 'Санкт-Петербург')]").click();
         $x("//span[contains(@class , 'input__icon')]").click();
-        $x("//div[(@data-step='1')]").click();
-        $x("//*[contains(@data-day, '1676415600000')]").click();
+        if (planingDate > currentDate) {
+            $x("//div[(@data-step='1')]").click();
+        }
+        $x(String.format("//td[contains(text(),'%s')]", planingDay)).click();
         $("[data-test-id='name'] input").setValue("Иванов Илья");
         $("[data-test-id='phone'] input").setValue("+79436578935");
         $x("//label[contains(@data-test-id, 'agreement')]").click();
         $x("//*[contains(text(), 'Забронировать')]").click();
         $x("//*[contains(@class,'notification__content')]").should(appear, Duration.ofMillis(15000));
-        $x("//*[contains(@class,'notification__content')]").shouldHave(Condition.text("Встреча успешно забронирована на " + "15.02.2023"), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+        $x("//*[contains(@class,'notification__content')]")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(7, "dd.MM.yyyy")),
+                        Duration.ofSeconds(15)).shouldBe(Condition.visible);
 
     }
 }
